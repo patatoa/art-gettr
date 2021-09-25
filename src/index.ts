5347d3a881cb32fa44eae36bca4019a5c1f57350
Patatoa <patatoa@gmail.com>
2021-09-25 15:50:33 -0500

    Can Save to db

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import AlbumRecord from './albumRecord';
 import AlbumRequest from './albumRequest';
 
 async function downloadImage(url:string, filepath:string): Promise<string> {
@@ -9,7 +10,7 @@ async function downloadImage(url:string, filepath:string): Promise<string> {
         method: 'GET',
         responseType: 'stream'
     });
-    const bucket = storage.bucket('mu-list-album-art-bkt');
+    const bucket = storage.bucket(process.env.GOOGLE_BUCKET_ID);
     const file = bucket.file(filepath);
 
     try {
@@ -20,7 +21,20 @@ async function downloadImage(url:string, filepath:string): Promise<string> {
     }
     return file.publicUrl();
 }
+async function saveToDb(request: AlbumRecord): Promise<void> {
+    const Firestore = require('@google-cloud/firestore');
 
+    const db = new Firestore({
+        projectId: process.env.GOOGLE_PROJECT_ID,
+    });
+    const docRef = db.collection('album').doc();
+    try {
+        await docRef.set(request);
+    }
+    catch (err:any) {
+        throw new Error(`Error Saving to DB. Message: ${err.message}`);
+    }
+}
 async function albumGettr(request: AlbumRequest): Promise<string> {
     const albumArt = require('album-art');
     try{
@@ -34,6 +48,12 @@ async function albumGettr(request: AlbumRequest): Promise<string> {
 }
 (async () => {
     const url:string = await albumGettr({artist: 'The Beatles', album: 'Abbey Road'});
-    const ourUrl = await downloadImage(url, 'test12345555.jpg');
+    const ourUrl = await downloadImage(url, 'testrrr4453.jpg');
+    const record: AlbumRecord = {
+        artist: 'The Beatles',
+        album: 'Abbey Road',
+        imagePath: ourUrl
+    };
+    await saveToDb(record);
 })();
 
