ca28bb97c015c6154c30a18bed4610bfde8faff5
Patatoa <patatoa@gmail.com>
2021-09-25 14:15:34 -0500

    saves to google cloud bucket

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,37 @@
-import fs from 'fs';
 import axios from 'axios';
 
-async function downloadImage(url:string, filepath:string) {
+async function downloadImage(url:string, filepath:string): Promise<void> {
+    const {Storage} = require('@google-cloud/storage');
+    const storage = new Storage();
     const response = await axios({
         url,
         method: 'GET',
         responseType: 'stream'
     });
-    return new Promise((resolve, reject) => {
-        response.data.pipe(fs.createWriteStream(filepath))
-            .on('error', reject)
-            .once('close', () => resolve(filepath)); 
-    });
+    const bucket = storage.bucket('mu-list-album-art-bkt');
+    const file = bucket.file(filepath);
+
+    try {
+        await response.data.pipe(file.createWriteStream());
+    }
+    catch (err:any) {
+        throw new Error(`Error Saving file to server. Message: ${err.message}`);
+    }
 }
 
 async function albumGettr(): Promise<string> {
     const albumArt = require('album-art');
-    // Download the url to a file
-    const albumUrl = await albumArt('Beatles', { album: 'Abbey Road' });
-    return albumUrl;
+    try{
+        const albumUrl = await albumArt('Beatles', { album: 'Abbey Road' });
+        return albumUrl;
+    }
+    catch (err:any) {
+        throw new Error(`Error finding album art. Message: ${err.message}`);
+        
+    }
 }
 (async () => {
     const url:string = await albumGettr();
-    downloadImage(url, './test.jpg');
+    await downloadImage(url, 'test12345555.jpg');
 })();
 
